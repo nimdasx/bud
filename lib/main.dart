91276bba43962c479319956c9bea6fcdd392baac
dev <dev@dev>
2024-01-25 15:50:30 +0700

    suara

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,44 @@
-import 'package:bud/page_favorit.dart';
-import 'package:bud/page_generator.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:english_words/english_words.dart';
-import 'package:provider/provider.dart';
+import "package:flutter/material.dart";
 
 void main() {
-  runApp(MyApp());
+  runApp(FatihApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FatihApp extends StatefulWidget {
+  const FatihApp({super.key});
+
+  @override
+  State<FatihApp> createState() => _FatihAppState();
+}
+
+class _FatihAppState extends State<FatihApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: Color.fromARGB(255, 104, 75, 101)),
-        ),
-        home: MyHomePage(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Drug BUD App"),
       ),
+      body: Center(
+        child: Text("Test"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.amber,
+          currentIndex: 1,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "profile"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart), label: "jangkrik")
+          ]),
     );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
-
-  var favorites = <WordPair>[];
-
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
-    } else {
-      favorites.add(current);
-    }
-    notifyListeners();
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var selectedIndex = 0;
-  @override
-  Widget build(BuildContext context) {
-    Widget page;
-    switch (selectedIndex) {
-      case 0:
-        page = GeneratorPage();
-        break;
-      case 1:
-        page = FavoritesPage();
-        break;
-      default:
-        throw UnimplementedError('no widget for $selectedIndex');
-    }
-    return LayoutBuilder(builder: (context, Constraints) {
-      return Scaffold(
-        body: Row(
-          children: [
-            SafeArea(
-              child: NavigationRail(
-                extended: Constraints.maxWidth >= 600,
-                destinations: [
-                  NavigationRailDestination(
-                    icon: Icon(Icons.home),
-                    label: Text('Home'),
-                  ),
-                  NavigationRailDestination(
-                    icon: Icon(Icons.favorite),
-                    label: Text('Favorites'),
-                  ),
-                ],
-                selectedIndex: selectedIndex,
-                onDestinationSelected: (value) {
-                  setState(() {
-                    selectedIndex = value;
-                  });
-                },
-              ),
-            ),
-            Expanded(
-              child: Container(
-                color: Theme.of(context).colorScheme.primaryContainer,
-                child: page,
-              ),
-            ),
-          ],
-        ),
-      );
-    });
   }
 }
